62131a31a10896bfb4f6c3f57a5f78de9b4e9e4a
jacobki <jacobki@kth.se>
2018-06-05 17:13:56 +0200

    Day 1

--- ols2yaml.py
+++ ols2yaml.py
@@ -1,42 +1,84 @@
 #!/usr/bin/env python
-
 import yaml
+import hashlib
+from collections import OrderedDict
 
-print("Hello world")
+def main():
+    stream = open('skylake_formated_clean.yaml', 'r')
+    data = yaml.safe_load(stream)
+    parsed = parser(data)
+    
+    #Prints
+    print (parsed)
 
-stream = file('skylake_formated_clean.yaml', 'r')
-data = yaml.safe_load(stream)
-noPorts = "Instructions without defined ports: \n"
+#Parse the .yaml file
+def parser(data):
+    noPorts = "No ports: \n"
+    undefPrefix = "Undefined prefixes: \n"
+    noRecThro = "No reciprocal throughput defined: \n"
 
-# Print instruction names
-for instruction in data:
-    uOps = instruction['Uops each port']
+    # Print instruction names
+    for instruction in data:
+        uOps = instruction['Uops each port']
+        reciprocalThroughput = instruction['Reciprocal throughput']
+        recThro = True
 
-    #Check for non-null
-    if uOps:
-        splitUOps = uOps.split(' ')
-        cardinality = 0
-        for ports in splitUOps:
-            #Check if first character defines parallel ports on the form of "2p0156"
-            prefix = ports[0]
+        if not reciprocalThroughput:
+            noRecThro += str(instruction) + "\n"
+            recThro = False
+            
+    
+        #Check for non-null
+        if uOps and recThro:
+            splitUOps = uOps.split(' ')
             cardinality = 0
-            #TODO fix prefix stuff, sometimes it is a n
-            if not isinstance(prefix, int):
-                print prefix
-            #If there is no division prefix, just calculate cardinality
-            if prefix == 'p':
-                cardinality = len(ports) - 1
-            #We have found a division prefix
-            else:
-                cardinality = len(ports) - 1 / int(prefix)
-                ports = ports[1:]
-                #print ports
+            for ports in splitUOps:
+                #Check if first character defines parallel ports on the form of "2p0156"
+                prefix = ports[0]
+                cardinality = 0
+                #Standard prefix
+                if (prefix == 'p'):
+                    cardinality = len(ports) - 1
+                #Numerical prefix
+                elif isNumber(prefix):
+                    ports = ports[1:]
+                    #cardinality = len(ports) - 1 / int(prefix)
+                #Undefined prefix
+                else:
+                    undefPrefix += str(instruction) + "\n"
+    
+        #Calculate actual throughput here
+    
+    
+        # Save all instructions without defined ports
+        else:
+            noPorts += str(instruction) + "\n"
 
-                
-                
-    # Save all instructions without defined ports
-    else:
-        noPorts += str(instruction) + "\n"
+        incompleteList = removeDuplicateEntries(noPorts + undefPrefix + noRecThro) 
+#        print (incompleteList)
 
-print noPorts
+#Remove duplicate lines in a string
+def removeDuplicateEntries(listString):
+    #Remove duplicate lines in list of incomplete instructions
+    listString.splitlines()
+    completedLinesHash = set()
+    uniqueList = ""
+    for line in listString:
+        hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+        if hashValue not in completedLinesHash:
+            completedLinesHash.add(hashValue)
+            uniqueList += line
+    return uniqueList
 
+#Check if a is a number
+def isNumber(a):
+    try:
+        int(a)
+        return True
+    except ValueError:
+        return False
+
+
+
+if __name__ == "__main__":
+    main()
