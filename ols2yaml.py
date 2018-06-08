92ef277eb6eea2277212f3cdbaeaa43a9d658ab6
jacobki <jacobki@kth.se>
2018-06-08 17:04:01 +0200

    Day 3

--- ols2yaml.py
+++ ols2yaml.py
@@ -12,95 +12,130 @@ def main():
 
 #Parse the .yaml file
 def parser(data):
-    noPorts = "No ports: \n"
-    undefPrefix = "Undefined prefixes: \n"
+    undefPorts = "Undefined ports: \n"
     noRecThro = "No reciprocal throughput defined: \n"
     incompleteList = "Inc. list: \n"
-    resourceUsageList = "Instructions - Operands - [Resource - Resoure Usage - Hold-time]"
+    resourceUsageList = "Instructions - Operands - [Resource - Resoure Usage - Hold-time] \n"
 
     # Print instruction names
     for instruction in data:
+
+        resources = str(instruction['Instruction']) + " - " + str(instruction['Operands']) + " - " + "["
+
         uOps = instruction['Uops each port']
         reciprocalThroughput = instruction['Reciprocal throughput']
         resourceUsage = []
 
-        if not reciprocalThroughput:
+        #Check if rec. thro. is none here, its needed later in calculations
+        if reciprocalThroughput is None:
             noRecThro += str(instruction) + "\n"
+            continue
 
-        #Check for non-null
-        if uOps:
-            #Check that ports are correctly defined according to RegEx
-            temp = str(isPortDefined(str(uOps)))
-            if str(isPortDefined(str(uOps))) == "None":
-                print("None: " + "\"" + uOps + "\"")
-                print("Temp: " + temp)
+        #Check that ports are correctly defined according to custom RegEx
+        match = isPortDefined(str(uOps))
+        if (not (match is None)) and match.end() - match.start() == len(uOps):
+            # print("Temp: " + str(match))
             
-            # print (str(uOps))
-            isPortDefined(str(uOps))
             splitUOps = uOps.split(' ')
             largestCard = 0
-            resource = ""
-
-            #Get the highest port-cardinality, we need this for further calculations
-            # try:
-                # largestCard = largestCardinality(splitUOps, instruction)
-            # except Exception as error:
-                # print("Error caught: " + repr(error) + "\n" + "Instruction used: " + instruction)
-
-            #Make the calculations
+                
+            print("splitUops: " + str(splitUOps))
+            print("splitUops len: " + str(len(splitUOps)))
+            
+            #Deal with load/store instructions and remove them from the splitUOps
+            iterations = 1
             for ports in splitUOps:
-                #Check if first character defines parallel ports on the form of "2p0156"
-                prefix = ports[0]
-                #Standard prefix
-                if (prefix == 'p'):
-                    #Instruction uses LD, STA, or STD (load/store Uop)
-                    if ports == "p23" or ports == "p237" or ports == "p4":
-                        resource += ports + "1" + "1"
-                    #Instruction is not a load/store
-                    # else:
+                print (iterations)
+                if isLoadStore(ports):  
+                    resources += str(ports) + " - 1 - " + "1, " 
+                    print("removing port: " + ports)
+                    splitUOps.remove(ports)
 
-                #Numerical prefix
-                elif isNumber(prefix):
-                    ports = ports[1:]
-                    #cardinality = len(ports) - 1 / int(prefix)
-                #Undefined prefix
-                else:
-                    undefPrefix += str(instruction) + "\n"
+            #Check largest cardinality of ports
+            if(splitUOps):
+                cardinality = largestCardinality(splitUOps)
+                print("UOps :" + str(splitUOps))
+                print("Largest card:" + str(cardinality))
+
+            #Make the throughput calculations
+            # for ports in splitUOps:
+                # #Check if first character defines parallel ports on the form of "2p0156"
+                # prefix = ports[0]
+                # #Standard prefix
+                # if (prefix == 'p'):
+                    # #Instruction uses LD, STA, or STD (load/store Uop)
+                    # if ports == "p23" or ports == "p237" or ports == "p4":
+                    # #Instruction is not a load/store
+                    # # else:
+
+                # #Numerical prefix
+                # elif isNumber(prefix):
+                    # ports = ports[1:]
+                    # #cardinality = len(ports) - 1 / int(prefix)
+                # #Undefined prefix
+                # else:
+                    # undefPrefix += str(instruction) + "\n"
+
         # Save all instructions without defined ports
         else:
-            noPorts += str(instruction) + "\n"
+            undefPorts += str(instruction) + "\n"
 
-        incompleteList += noPorts + undefPrefix + noRecThro 
+        #Save undefined ports and instructions without througput
+
+        resourceUsageList += resources[:-2] + "]" "\n"
+
 
     #Some instructions are added twice to some lists, as they brake the same rule twice. Thus we remove all duplicate entries in all lists.
-    noPorts = removeDuplicateEntries(noPorts)
-    undefPrefix = removeDuplicateEntries(undefPrefix)
+    undefPorts = removeDuplicateEntries(undefPorts)
     noRecThro = removeDuplicateEntries(noRecThro)
 
     #Print the list
-    print (noPorts + undefPrefix + noRecThro)
+    print (resourceUsageList + undefPorts + noRecThro)
 
 #Find the largest cardinality in a string of port-definitions
-def largestCardinality (ports, instruction):
+def largestCardinality (ports):
     largestCard = 0
     for port in ports:
-        prefix = port[0]
-        #Standard prefix
-        if (prefix == 'p'):
-            if largestCard < len(port) - 1:
-                largestCard = len(port) - 1
+        #Remove numerical prefix
+        port = removePrefix(port)
 
-        #Numerical prefix
-        elif(isNumber(prefix)):
-            #We want the cardinality of the numbers following p
-            purgeP = port.split('p')
-            if largestCard < len(purgeP[1]):
-                largestCard = len(purgeP[1])
-            # else:
-                # raise {0,1}Exception("Ports used by Uops are incorrectly defined for instruction: " + instruction)
-
+        #Check cardinality
+        if largestCard < len(port) - 1:
+            largestCard = len(port) - 1
+        
     return largestCard
 
+#Removes the numerical prefix (if existent) in front of ports
+def removePrefix(ports):
+    prefix = ports[0]
+    #Remove numerical prefix
+    while isNumber(prefix):
+        ports = ports[1:]
+        prefix = ports[0]
+
+    return ports
+
+#Receive a single group of ports (p237) and see if they are used for load or store operations
+def isLoadStore(port):
+    #Remove numerical prefix
+    prefix = port[0]
+    while isNumber(prefix):
+        port = port[1:]
+        prefix = port[0]
+    if port == "p23" or port == "p237" or port == "p4":
+        return True
+    
+    else:
+        return False
+
+#Check if an instruction solely consists of load/store ports
+def isExclusivelyLoadStore(ports):
+    exclusive = True
+    for port in ports:
+        if not isLoadStore(port):
+            exclusive = False
+    
+    return exclusive
 
 #Remove duplicate lines in a string
 def removeDuplicateEntries(listString):
@@ -111,8 +146,8 @@ def removeDuplicateEntries(listString):
 def isPortDefined(ports):
     #Regular expression for prefix_opt-'p'-ports-opt_whitespace, all repeated at least once
     regEx = re.compile('([\d]*[p][\d]+[\s]{0,1})+')
-    match = regEx.match(ports)
-    return match
+    regMatch = regEx.match(ports)
+    return regMatch
 
 #Check if parameter is a number
 def isNumber(a):
