0e04553229cac081862c220285e4a125e34ddc01
jacobki <jacobki@kth.se>
2018-06-19 10:55:40 +0200

    Find matching instructions and regular expressions

--- tablegen/tableGenParse.py
+++ tablegen/tableGenParse.py
@@ -2,47 +2,72 @@
 
 import yaml
 import pyparsing as pp
+import re
 
 #Separate file with a list containing all x86 instructions defined for unison
 from unison_instructions import unisonInstructions
 
-#Transform string of unison instructions to a list of strings with individual instructions
-instructions = unisonInstructions.replace("\n", '')
-instructions = instructions.replace("\t", '')
-instructions = instructions.replace(" ", '')
-instructions = instructions.split(",")
-print (instructions)
+def main():
+    #Transform string of unison instructions to a list of strings with individual instructions
+    uniInstr = unisonInstructions.replace("\n", '')
+    uniInstr = uniInstr.replace("\t", '')
+    uniInstr = uniInstr.replace(" ", '')
+    uniInstr = uniInstr.split(",")
 
-writeResGroup = pp.Word(pp.alphanums)
-instRegex = pp.SkipTo("\"")
 
-#Parser to find each regex-instructions that belongs to a SKLWriteResGroup
-instrGroupDef = pp.Suppress("def: InstRW<[") + writeResGroup("SKLWriteResGroup") + pp.Suppress("], (instregex \"") + instRegex("Regex") + pp.Suppress("\")>;")
+    #Parser to find each regex-instructions that belongs to a SKLWriteResGroup
+    writeResGroup = pp.Word(pp.alphanums)
+    instRegex = pp.SkipTo("\"")
+    instrGroupDef = pp.Suppress("def: InstRW<[") + writeResGroup("SKLWriteResGroup") + pp.Suppress("], (instregex \"") + instRegex("Regex") + pp.Suppress("\")>;")
 
-ports = pp.SkipTo("]")
-latency = pp.Word(pp.nums)
-microOps = pp.Word(pp.nums)
-resourceCycles = pp.SkipTo("]")
-# lineEnd = pp.Suppress(pp.LineEnd)
-#Parser to find each SKLWriteResGroup definition
-sKLWriteResGroupDef = pp.Suppress("def ") + writeResGroup("SKLWriteResGroup") + pp.Suppress(": SchedWriteRes<[") + ports("Ports") + pp.Suppress(pp.restOfLine) + (
-        pp.Suppress("let Latency = ") + latency("Latency")  + pp.Suppress(pp.restOfLine) +
-        pp.Suppress("let NumMicroOps = ") + microOps("NumMicroOps") + pp.Suppress(pp.restOfLine) + 
-        pp.Suppress("let ResourceCycles = [") + resourceCycles("ResourceCycles") + pp.Suppress(pp.restOfLine)
-    )
+    #Parser to find each SKLWriteResGroup definition
+    ports = pp.SkipTo("]")
+    latency = pp.Word(pp.nums)
+    microOps = pp.Word(pp.nums)
+    resourceCycles = pp.SkipTo("]")
+    sKLWriteResGroupDef = pp.Suppress("def ") + writeResGroup("SKLWriteResGroup") + pp.Suppress(": SchedWriteRes<[") + ports("Ports") + pp.Suppress(pp.restOfLine) + (
+            pp.Suppress("let Latency = ") + latency("Latency")  + pp.Suppress(pp.restOfLine) +
+            pp.Suppress("let NumMicroOps = ") + microOps("NumMicroOps") + pp.Suppress(pp.restOfLine) + 
+            pp.Suppress("let ResourceCycles = [") + resourceCycles("ResourceCycles") + pp.Suppress(pp.restOfLine)
+        )
 
-SKLWriteResGroups = []
-instructions = []
+    SKLWriteResGroups = []
+    instructions = []
 
-for SKLWriteResGroup in sKLWriteResGroupDef.searchString(open('X86SchedSkylakeClient.td').read()):
-    SKLWriteResGroups.append(SKLWriteResGroup.asDict())
+    for SKLWriteResGroup in sKLWriteResGroupDef.searchString(open('X86SchedSkylakeClient.td').read()):
+        SKLWriteResGroups.append(SKLWriteResGroup.asDict())
 
-for instrGroup in instrGroupDef.searchString(open('X86SchedSkylakeClient.td').read()):
-    instructions.append(instrGroup.asDict())
+    for instrGroup in instrGroupDef.searchString(open('X86SchedSkylakeClient.td').read()):
+        instructions.append(instrGroup.asDict())
 
-# print (unison_instructions)
-# print ("Skylake WriteResGroups: ")
-# print (yaml.dump(SKLWriteResGroups, default_flow_style = False))
-# print ("Skylake instructions: ")
-# print (yaml.dump(instructions, default_flow_style = False))
 
+    #Extract all regex
+    regexList = []
+    for instruction in instructions:
+        regexList.append(instruction['Regex'])
+
+    #Match instructions and regex
+    matchings = instructionMatching(uniInstr, regexList)
+    print(matchings)
+
+
+#Find out if there are any instructions not matched to the instructions defined in unison
+def instructionMatching (instructions, regexList):
+    matchings = {
+            'Matched' :  [],
+            'Unmatched' : []
+            }
+
+    for instruction in instructions:
+        matched = False
+        for regex in regexList:
+            searchResult = re.search(regex, instruction) 
+            if searchResult is (not None):
+                matched = True
+                matchings['Matched'].append(instruction)
+
+        if not matched:
+            matchings['Unmatched'].append(instruction)
+
+if __name__ == '__main__':
+    main()
